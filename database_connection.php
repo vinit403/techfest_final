d144e1b94673aedcc519c9252517d22fd63dbebf
Jils vaghasiya <jilsvaghasiya333@gmail.com>
2022-04-01 08:57:00 +0530

    21 commit

--- database_connection.php
+++ database_connection.php
@@ -5,9 +5,9 @@
         // $database = "techfest_website_database";
         // $connect = mysqli_connect($servername, $username, $password, $database); 
 
-        $servername = "techfest.c4vg6z6fzyif.ap-south-1.rds.amazonaws.com";
+        $servername = "techpulse.crqeegwkwobj.ap-south-1.rds.amazonaws.com";
         $username = "admin";
-        $password = "techfest#12";
-        $database = "techfest";
-        $connect = mysqli_connect($servername, $username, $password, $database); 
+        $password = "Techfest##90";
+        $database = "techpulse";
+        $connect = mysqli_connect($servername, $username, $password, $database);  
 ?>
