6f3ed39c47e96114d3962592a910637e24f895fc
ayush rajgor <ayushrajgorar@gmail.com>
2022-04-04 13:06:28 +0530

    howto page add

--- page_navbar.php
+++ page_navbar.php
@@ -13,7 +13,12 @@ if(isset($_SESSION['logged_in']))
             <div class="row">
                 <div class="col-md-12">
                     <!-- logo begin -->
+                        <nav>
+                        <ul id="mainmenu" class="ms-2">
+                                <li><a href="index.php">Home<span></span></a></li>
+</ul>
 
+                        </nav>
                     <!-- logo close -->
 
                     <!-- small button begin -->
@@ -28,6 +33,7 @@ if(isset($_SESSION['logged_in']))
                                 <li><a href="Event.php">Events<span></span></a></li>
                                 <li><a href="package.php">Packages<span></span></a></li>
                                 <li><a href="contact-us.php">Contact us<span></span></a></li>
+                                <li><a href="howto.php">How to?<span></span></a></li>
                                 <li><a href="accomodation.php">Accomodation<span></span></a></li>
                                 <li><a href="dashboard.php" style="color:#ec167f";>'.$user_id.'<span></span></a></li>
                                 <li><a href="logout_user.php" style="color:#ec167f">logout<span></span></a></li>
@@ -90,5 +96,3 @@ if($flag == 1)
 </div>
 </header>';
 }
-
-?>
