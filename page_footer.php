3ca307dca44d3cdab29e469c6766b986da34e8e8
ayush rajgor <ayushrajgorar@gmail.com>
2022-04-17 22:05:17 +0530

    css responsive

--- page_footer.php
+++ page_footer.php
@@ -9,13 +9,10 @@
 
 </style>
 <?php
-    echo '  <footer class="style-2" style="background:#18191b">
+    echo '  <footer class="style-2">
     <div class="container">
         <div class="row align-items-middle">
-            <div class="col-md-3">
-                <a href="index.php"><div class="img"><img class="logo" src="images-event/techpulse-logo.png" alt="" style="height: 50px;width: 190px;"></div></a>
-            </div>
-
+            
             <div class="col-md-6" style="text-align: center;">
                  © <span class="id-color"> Tech-Pulse 2022-23 </span>All rights reserved.<br><a href="https://www.google.com/maps/place/P+P+Savani+University/@21.4982445,73.0058995,17z/data=!3m1!4b1!4m5!3m4!1s0x3be03c88456d850b:0x30dc0473d5cd8b53!8m2!3d21.4982445!4d73.0080882">NH 8, GETCO, Near Biltech,Village: Dhamdod, Kosamba,Dist.: Surat - 394125,Gujarat.</a>
             </div>
