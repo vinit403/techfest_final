fece3ca3e208e864a68ea359fe14aed175ec7c80
ayush rajgor <ayushrajgorar@gmail.com>
2022-04-17 18:40:49 +0530

    css

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
         <div id="content" class="no-bottom no-top">
 
             <!-- revolution slider begin -->
-            <section id="section-hero" class="full-height text-light" data-bgimage="url(images/main-page.jpg) fixed top center" data-stellar-background-ratio=".2">
+            <section id="section-hero" class="full-height text-light" data-bgimage="url(images/main-page.jpg) fixed top center" data-stellar-background-ratio=".2" style="position: relative;">
             <div id="particles-js"></div>
 				<div class="center-y text-center">
 					<div class="container">
