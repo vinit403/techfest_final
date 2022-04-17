5ef25df439a6fbcd513b25ecf2ce86d2283f00b1
ayush rajgor <ayushrajgorar@gmail.com>
2022-04-17 11:52:03 +0530

    smtp creds

--- smtp.php
+++ smtp.php
@@ -1,11 +1,17 @@
 <?php
 
-    $usernameSmtp = 'postmaster@techpulse.co.in';
+    // $usernameSmtp = 'postmaster@techpulse.co.in';
 
-    $passwordSmtp = '0d2c7512b24f0f56a9a5d696b92334e4-38029a9d-55ea6358';
+    // $passwordSmtp = '0d2c7512b24f0f56a9a5d696b92334e4-38029a9d-55ea6358';
 
-    $host = 'smtp.mailgun.org';
+    // $host = 'smtp.mailgun.org';
+    // $port = 587;
+
+    $usernameSmtp = 'MS_a0yFUo@techpulse.co.in';
+
+    $passwordSmtp = 'f0MX0IiMy0xI9fov';
+
+    $host = 'smtp.mailersend.net';
     $port = 587;
-
 
 ?>
