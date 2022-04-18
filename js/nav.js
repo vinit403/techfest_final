5b7c24f69f39ce8de473f3227b5a60c3c3a9ad33
ayush rajgor <ayushrajgorar@gmail.com>
2022-04-18 10:00:12 +0530

    responsive nav

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,14 @@
-// if (document.documentElement.clientWidth < 900) {
-//   var prevScrollpos = window.pageYOffset;
+if (document.documentElement.clientWidth < 900) {
+  var prevScrollpos = window.pageYOffset;
 
-//   window.addEventListener("scroll", function () {
-//     var currentScrollPos = window.pageYOffset;
-//     header = document.querySelector("header");
-//     if (prevScrollpos > currentScrollPos) {
-//       header.style.top = "0";
-//     } else {
-//       header.style.top = "-80px";
-//     }
-//     prevScrollpos = currentScrollPos;
-//   });
-// }
+  window.addEventListener("scroll", function () {
+    var currentScrollPos = window.pageYOffset;
+    header = document.querySelector("header");
+    if (prevScrollpos > currentScrollPos) {
+      header.style.top = "0";
+    } else {
+      header.style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+  });
+}
